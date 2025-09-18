8ab2ff44172e42db93fe841a11fce6df42ceba5e
sifat1 <sifatshikdhar@gmail.com>
2025-09-18 19:31:06 +0600

    init

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    [StringLength(100)]
+    public string Name { get; set; }
+    [StringLength(500)]
+    public string Description { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
+    public string? ImageUrl { get; set; }
+
+    [ForeignKey("Category")]
+    public int CategoryId { get; set; }
+    public required Category Category { get; set; }
+}
