7e92447ec17cf5d3ca87197fa06edab03e1d895d
sifat1 <sifatshikdhar@gmail.com>
2025-09-18 19:56:23 +0600

    added catagory model

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [StringLength(100)]
     public string Name { get; set; }
+    [StringLength(500)]
     public string Description { get; set; }
 }
